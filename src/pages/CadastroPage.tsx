6054bf34aeec3d74c90329390fb2b6d91ef32bf5
Talles <talves2503@gmail.com>
2025-07-10 01:56:34 -0300

    feat: register endpoint

--- src/pages/CadastroPage.tsx
+++ src/pages/CadastroPage.tsx
@@ -4,20 +4,21 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import backgroundImage from "../assets/car-repair-background.webp";
 import Input from "../components/shared/Input";
+import api from "../services/api";
 
 const schema = yup.object().shape({
   email: yup
     .string()
     .required("E-mail é obrigatório")
     .email("E-mail deve ter um formato válido"),
-  senha: yup
+  password: yup
     .string()
     .required("Senha é obrigatória")
     .min(6, "Senha deve ter pelo menos 6 caracteres"),
-  confirmarSenha: yup
+  confirmPassword: yup
     .string()
     .required("Confirmação de senha é obrigatória")
-    .oneOf([yup.ref("senha")], "Senhas devem ser iguais")
+    .oneOf([yup.ref("password")], "Senhas devem ser iguais"),
 });
 
 type FormData = yup.InferType<typeof schema>;
@@ -27,12 +28,22 @@ export default function CadastroPage() {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm<FormData>({
-    resolver: yupResolver(schema)
+    resolver: yupResolver(schema),
   });
 
-  const onSubmit = (data: FormData) => {
-    console.log(data);
+  const onSubmit = async (data: FormData) => {
+    try {
+      await api.post("/register", {
+        email: data.email,
+        password: data.password,
+      });
+
+      reset();
+    } catch (error) {
+      console.error("Erro ao cadastrar usuário", error);
+    }
   };
 
   return (
@@ -50,27 +61,30 @@ export default function CadastroPage() {
       {/* form */}
       <section className="flex flex-col gap-7 justify-center px-16 bg-secondary w-2/3  text-white">
         <h1 className="text-white font-bold text-4xl">Cadastre-se</h1>
-        <form className="flex flex-col w-full gap-5" onSubmit={handleSubmit(onSubmit)}>
-          <Input 
+        <form
+          className="flex flex-col w-full gap-5"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Input
             placeholder="E-mail"
             type="email"
             {...register("email")}
             error={errors.email?.message}
           />
-          <Input 
-            placeholder="Senha" 
+          <Input
+            placeholder="Senha"
             type="password"
-            {...register("senha")}
-            error={errors.senha?.message}
+            {...register("password")}
+            error={errors.password?.message}
           />
-          <Input 
-            placeholder="Confirme sua senha" 
+          <Input
+            placeholder="Confirme sua senha"
             type="password"
-            {...register("confirmarSenha")}
-            error={errors.confirmarSenha?.message}
+            {...register("confirmPassword")}
+            error={errors.confirmPassword?.message}
           />
 
-          <button 
+          <button
             type="submit"
             className="bg-primary-light text-xl font-bold rounded-xl min-w-1/3 self-center py-5 mt-4 hover:scale-105 transition-all cursor-pointer"
           >
